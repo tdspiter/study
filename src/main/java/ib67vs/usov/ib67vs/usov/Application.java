b64da295302f4cfe470395378cdd6b8a5cb69e76
tdspiter <tdspiter@yandex.ru>
2019-10-20 17:35:26 +0300

    first2

---
+++ src/main/java/ib67vs/usov/ib67vs/usov/Application.java
@@ -0,0 +1,13 @@
+package ib67vs.usov.ib67vs.usov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
